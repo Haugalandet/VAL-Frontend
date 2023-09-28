d0a033e0eb266868308a2380452c7bf3e6e246d7
Neelzee <nilsien2001@gmail.com>
2023-09-28 16:52:08 +0200

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { LandingPage } from "./pages/landingpage";
+import { Route, Routes } from "react-router";
+import { Dashboard } from "./pages/dashboard";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,19 @@
+import { Navbar } from "../components/navbar";
+
+export function Dashboard() {
+  return (
+    <>
+    <header>
+      <Navbar />
+    </header>
+    <body>
+      <h1>Dashboard</h1>
+    </body>
+    <footer>
+      <p>
+        VAL 2023
+      </p>
+    </footer>
+    </>
+  );
+}
