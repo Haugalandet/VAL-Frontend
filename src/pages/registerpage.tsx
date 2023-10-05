b6baecd53bd8be9af7fbd4781e656f523341c05e
Helene Solhaug <54099085+h586613@users.noreply.github.com>
2023-10-05 15:25:17 +0200

    added form and style (pizzazz) to register page

--- src/pages/registerpage.tsx
+++ src/pages/registerpage.tsx
@@ -1,19 +1,38 @@
 import { NavLink } from "react-router-dom";
 import { Footer } from "../components/footer";
 import { Navbar } from "../components/navbar";
+import "../styles/register.scss";
 
 export function RegisterPage() {
-    return (
-        <>
-        <header>
-            <Navbar />
-        </header>
-        <NavLink to="/login">
-            <button>
-                Register user
-            </button>
-        </NavLink>
-        <Footer />
-        </>
-    );
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <form action="">
+          <article>
+            <div className="labeldiv">
+              <label htmlFor="uname">Username: &nbsp;</label>
+              <br />
+              <label htmlFor="passwd1">Password: &nbsp;</label>
+              <br />
+              <label htmlFor="passwd2">Repeat password: &nbsp;</label>
+            </div>
+            <div>
+              <input type="text" name="uname" />
+              <br />
+              <input type="password" name="passwd1" />
+              <br />
+              <input type="password" name="passwd2" />
+            </div>
+          </article>
+          <br />
+          <button>Register</button>
+          <br />
+        </form>
+      </main>
+      <Footer />
+    </>
+  );
 }
