15c08388a83835885604a9c83e801d8f93168188
Neelzee <nilsien2001@gmail.com>
2023-11-09 17:12:16 +0100

    fixed issue #10

--- src/pages/votepage.tsx
+++ src/pages/votepage.tsx
@@ -3,7 +3,7 @@ import { Footer } from "../components/footer";
 import { Navbar } from "../components/navbar";
 import axios, { AxiosRequestConfig } from "axios";
 import { ApiRoot } from "../utils/consts";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import "../styles/home.scss";
 import { RenderPollVote } from "../components/render_poll";
 import { defaultPoll } from "../utils/funcs";
@@ -13,6 +13,7 @@ export function VotePage() {
   const { id } = useParams<{ id: string }>();
   const [poll, setPoll] = useState(defaultPoll());
   const [cookie] = useCookies(["Authorization"]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const config: AxiosRequestConfig = {
@@ -24,18 +25,22 @@ export function VotePage() {
       // @ts-ignore
       .get(ApiRoot(`polls/${id}`), config)
       .then((res) => {
+        if (res.data.hasUserVoted && res.data.needLogin) {
+          navigate(`/polls/${res.data.pollId}/view`);
+        }
+
         setPoll(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [cookie, id]);
+  }, [cookie, id, navigate]);
 
   /*
     TODO: Create error vote page
   */
-  if (poll.choices.length === 0) {
-    //return <Navigate to={`poll/${poll.id}`} />;
+  if (poll.pollId === undefined) {
+    navigate(`/poll`);
   }
 
   return (
