19e87e13a9bacb3a799fc0665b9306bc8baddf28
Neelzee <nilsien2001@gmail.com>
2023-11-12 20:03:01 +0100

    if logged in, home takes the user too the dashboard

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,11 +3,18 @@ import "../styles/home.scss";
 import { NavLink } from "react-router-dom";
 
 import logo from "../assets/logo.png";
+import { useCookies } from "react-cookie";
 
 export function Navbar() {
+  const [cookie] = useCookies(["Authorization"]);
+
+  const isLoggedIn = () => {
+    return cookie["Authorization"] !== undefined;
+  };
+
   return (
     <nav>
-      <NavLink to="/" className="logo-link">
+      <NavLink to={!isLoggedIn() ? "/" : "/dashboard"} className="logo-link">
         <img src={logo} alt="VAL Logo" />
       </NavLink>
       <div className="title">Poloins AS</div>
